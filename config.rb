3febf69ac66727ca9ac597d1c9bd5a726f2e62e3
Peter T Bosse II <ptb@ioutime.com>
2016-02-19 11:42:02 -0500

    Modify file 'config.rb'

--- config.rb
+++ config.rb
@@ -16,7 +16,7 @@ end
 
 Slim::Engine.set_options attr_quote: "'", :format => :xhtml, pretty: true, sort_attrs: true, shortcut: {'@' => {attr: 'role'}, '#' => {attr: 'id'}, '.' => {attr: 'class'}, '%' => {attr: 'itemprop'}, '&' => {tag: 'input', attr: 'type'}}
 
-set :sass, cache: false, output_style: :compressed
+set :sass, cache: false, style: :compressed
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
