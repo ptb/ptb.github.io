fa70728700eb26ab4c670bfb86addc277a301047
Peter T Bosse II <ptb@ioutime.com>
2016-02-19 11:11:51 -0500

    Modify file 'config.rb'

--- config.rb
+++ config.rb
@@ -15,7 +15,6 @@ exts.each do |ext|
 end
 
 Slim::Engine.set_default_options attr_quote: "'", :format => :xhtml, pretty: true, sort_attrs: true, shortcut: {'@' => {attr: 'role'}, '#' => {attr: 'id'}, '.' => {attr: 'class'}, '%' => {attr: 'itemprop'}, '&' => {tag: 'input', attr: 'type'}}
-::Compass.configuration.sass_options = { cache: false, line_comments: false, style: :expanded }
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
