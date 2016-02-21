5b95f1cdeaa66dbbd3406289be7429dd08d1cc3b
Peter T Bosse II <ptb@ioutime.com>
2016-02-21 18:11:58 -0500

    Modify file 'config.rb'

--- config.rb
+++ config.rb
@@ -1,18 +1,16 @@
-Time.zone = 'America/New_York'
+set :css_dir, 'css' if File.directory? 'source/css/'
+set :images_dir, 'img' if File.directory? 'source/img/'
+set :js_dir, 'js' if File.directory? 'source/js/'
 
-set :css_dir, 'css' if File.directory?('source/css/')
-set :images_dir, 'img' if File.directory?('source/img/')
-set :js_dir, 'js' if File.directory?('source/js/')
+ignore 'intros/*' if File.directory? 'source/intros/'
+ignore 'partials/*' if File.directory? 'source/partials/'
 
-set :partials_dir, 'partials' if File.directory?('source/partials/')
-
-ignore 'intros/*' if File.directory?('source/intros/')
-
-# set :layout, 'minimum'
-exts = %w[atom css json rss txt xml]
-exts.each do |ext|
+# set :layout, '_auto_layout'
+%w[atom css json rss txt xml].each do |ext|
   page "*.#{ext}", layout: false
 end
+
+Time.zone = 'America/New_York'
 
 activate :blog do |blog|
   blog.prefix = 'blog'
@@ -37,31 +35,50 @@ activate :blog do |blog|
   blog.page_link = 'page-:num'
 end
 
-Slim::Engine.set_options attr_quote: "'", :format => :xhtml, pretty: true, sort_attrs: true, shortcut: {'@' => {attr: 'role'}, '#' => {attr: 'id'}, '.' => {attr: 'class'}, '%' => {attr: 'itemprop'}, '&' => {tag: 'input', attr: 'type'}}
-
 set :index_file, 'index.html'
-set :relative_links, true
-set :strip_index_file, false
-
 activate :directory_indexes
-activate :relative_assets
 
 configure :development do
+  # activate :livereload, apply_js_live: false, no_swf: true
+  set :port, 5000
+  set :sass_source_maps, false
   set :sass, cache: false, line_comments: false, style: :expanded
+  set :slim,
+    attr_quote: "'",
+    format: :xhtml,
+    pretty: true,
+    sort_attrs: true,
+    shortcut: {
+      '@' => { attr: 'role' },
+      '#' => { attr: 'id' },
+      '.' => { attr: 'class' },
+      '%' => { attr: 'itemprop' },
+      '&' => { attr: 'type', tag: 'input' } }
+  set :strip_index_file, false
 
-  # activate :livereload
+  # url_for('/blog/file.html') or url_for(sitemap.resources[0])
+  # Example: style[link="#{url_for('/css/style.css')}" rel='stylesheet']
+  set :relative_links, true
+  activate :relative_assets
 end
 
-configure :build do
-  Slim::Engine.set_options pretty: false
-
-  # activate :minify_css
+configure :production do
+  set :port, 5001
   set :sass, cache: false, line_comments: false, style: :compressed
-
-  # activate :minify_javascript
+  set :slim,
+    attr_quote: "'",
+    format: :xhtml,
+    pretty: false,
+    sort_attrs: true,
+    shortcut: {
+      '@' => { attr: 'role' },
+      '#' => { attr: 'id' },
+      '.' => { attr: 'class' },
+      '%' => { attr: 'itemprop' },
+      '&' => { attr: 'type', tag: 'input' } }
 
   activate :asset_hash
-
+  activate :minify_css
   activate :minify_html, remove_quotes: false, simple_boolean_attributes: false
-  # activate :gzip, exts: %w(.atom .css .html .js .rss .svg .txt .xhtml .xml .eot .otf .ttf)
+  activate :minify_javascript
 end
